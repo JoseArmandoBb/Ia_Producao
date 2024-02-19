9139c4075aa0496f831358f8ffc6848d0f3c0b39
Armando <armandobarbosa489@gmail.com>
2024-02-18 21:15:41 -0300

    Comentario

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Producao do curso de ia em producao',
+    author='Armando',
+    license='MIT',
+)
